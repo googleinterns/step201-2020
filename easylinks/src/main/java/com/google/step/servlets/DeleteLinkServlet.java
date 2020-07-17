f3991772ee48fd8a449f0c3baeac2f3fa1189757
difanyang <1006380730@qq.com>
2020-07-17 08:11:03 +0000

    Add Delete functionality.

---
+++ easylinks/src/main/java/com/google/step/servlets/DeleteLinkServlet.java
@@ -0,0 +1,23 @@
+package com.google.step.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Servlet responsible for deleting links. */
+@WebServlet("/delete-link")
+public class DeleteLinkServlet extends HttpServlet {
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    long id = Long.parseLong(request.getParameter("id"));
+    Key linkEntityKey = KeyFactory.createKey("Link", id);
+    DEFAULT_DATASTORE_SERVICE.delete(linkEntityKey);
+  }
+}
--- easylinks/src/main/webapp/script.js
+++ easylinks/src/main/webapp/script.js
@@ -1,1 +1,8 @@
 /** Script code for the EasyLink project */
+
+/** Tells the server to delete the link. */
+function deleteLink(id) {
+  const params = new URLSearchParams();
+  params.append('id', id);
+  fetch('/delete-link', {method: 'POST', body: params});
+}
