4e10285a0cf124a1a07b5f4c1544714bbbdc46d9
Jiaxi Chen <jiaxi312@utexas.edu>
2020-07-17 02:22:55 +0000

    Add a helper class that could contains the commonly used helper method.

---
+++ easylinks/src/main/java/com/google/step/servlets/ServletHelper.java
@@ -0,0 +1,30 @@
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.gson.Gson;
+import javax.servlet.http.HttpServletRequest;
+
+/** Class contains the common used helper method for servlets.*/
+public final class ServletHelper {
+
+  /* The static store service that can be used to all servlets */
+  protected final static DatastoreService DEFAULT_DATASTORE_SERVICE
+                            = DatastoreServiceFactory.getDatastoreService();
+  protected final static Gson GSON = new Gson();
+
+  /** 
+   * @return the request parameter, or the default value if the parameter
+   *         was not specified by the client
+   */
+  protected static String getParameterWithDefault(HttpServletRequest request, String name, String defaultValue) {
+    String value = request.getParameter(name);
+    if (value == null || value.isEmpty()) {
+      return defaultValue;
+    }
+    return value;
+  }
+
+  /** Disables the default constructor */
+  private ServletHelper() {}
+}
