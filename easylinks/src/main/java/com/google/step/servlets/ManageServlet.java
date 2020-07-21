4d95f2c200d2fa8a0f4a8ba2ac4f0e44c3e41071
difanyang <difanyang@google.com>
2020-07-21 02:30:37 +0000

    Link delete feature to the button.

--- easylinks/src/main/java/com/google/step/servlets/ManageServlet.java
+++ easylinks/src/main/java/com/google/step/servlets/ManageServlet.java
@@ -35,9 +35,12 @@ public class ManageServlet extends HttpServlet {
   }
 
   @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    long id = Long.parseLong(request.getParameter("id"));
-    Key linkEntityKey = KeyFactory.createKey("Link", id);
-    ServletHelper.DEFAULT_DATASTORE_SERVICE.delete(linkEntityKey);
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException { 
+    final String action = ServletHelper.getParameterWithDefault(request, "action", "");
+    if (action == "delete"){
+      long id = Long.parseLong(request.getParameter("id"));
+      Key linkEntityKey = KeyFactory.createKey("Link", id);
+      ServletHelper.DEFAULT_DATASTORE_SERVICE.delete(linkEntityKey);
+    }
   }
 }
